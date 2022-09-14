3e86a1573a9941a4f942c63c3a0dd3d19303585d
Kivalchi <nwanyac.k@gmail.com.com>
2022-09-14 01:22:58 -0700

    holberton

--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -1,25 +1,23 @@
-int _putchar(char c);
+#include "holberton.h"
 
-void print_alphabet(void);
+/**
+ * main - check the code for Holberton School students.
+ *
+ *
+ *
+ * Return: Always 0.
+ *
+ */
 
-void print_alphabet_x10(void);
+int main(void)
 
-int _islower(int c);
-
-int _isalpha(int c);
-
-int print_sign(int n);
-
-int _abs(int n);
-
-int print_last_digit(int n);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int a, int b);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
+{
+print_times_table(3);
+_putchar('\n');
+print_times_table(5);
+_putchar('\n');
+print_times_table(98);
+_putchar('\n');
+print_times_table(12);  
+return (0);
+}
