70383763213cd29458a9ad6c60b955b334864a70
Kivalchi <nwanyac.k@gmail.com.com>
2022-10-03 02:08:51 -0700

    0-create_array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,6 +17,7 @@ char *create_array(unsigned int size, char c)
 {
 	char *cr;
 	unsigned int i;
+
 	if (size == 0)
 	return (NULL);
 	cr = malloc(sizeof(c) * size);
@@ -25,6 +26,5 @@ char *create_array(unsigned int size, char c)
 	for (i = 0; i < size; i++)
 	cr[i] = c;
 	return (cr);
-
 }
 
